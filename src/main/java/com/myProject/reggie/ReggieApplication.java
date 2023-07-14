e6b0bea9b7dcd70038fa6e6ec8608f1470ccd68f
kevingit777 <kevinwu0316@gmail.com>
2023-07-14 16:24:50 +0800

    add decorator for enable the login filter

--- src/main/java/com/myProject/reggie/ReggieApplication.java
+++ src/main/java/com/myProject/reggie/ReggieApplication.java
@@ -2,12 +2,13 @@ package com.myProject.reggie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootApplication
-
+@ServletComponentScan
 public class ReggieApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReggieApplication.class, args);
