677c9c79f2482297bb17c6525b2560779a96af43
kevingit777 <kevinwu0316@gmail.com>
2023-07-19 00:25:17 +0800

    add Mapper annotation

--- src/main/java/com/myProject/reggie/mapper/DishMapper.java
+++ src/main/java/com/myProject/reggie/mapper/DishMapper.java
@@ -1,8 +1,11 @@
 package com.myProject.reggie.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.myProject.reggie.entity.Dish;
 
+@Mapper
 public interface DishMapper extends BaseMapper<Dish> {
 
 }
--- src/main/java/com/myProject/reggie/mapper/SetmealMapper.java
+++ src/main/java/com/myProject/reggie/mapper/SetmealMapper.java
@@ -1,8 +1,11 @@
 package com.myProject.reggie.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.myProject.reggie.entity.Setmeal;
 
+@Mapper
 public interface SetmealMapper extends BaseMapper<Setmeal> {
 
 }
