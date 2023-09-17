a44510d1ac7cdbe7a3c0462f394c1a7c72e238f5
kevingit777 <kevinwu0316@gmail.com>
2023-09-17 14:38:31 -0500

    remove constructor

--- src/main/java/com/myProject/reggie/controller/OrderDetailController.java
+++ src/main/java/com/myProject/reggie/controller/OrderDetailController.java
@@ -10,8 +10,5 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/orderdetail")
 public class OrderDetailController {
 
-	public OrderDetailController() {
-		// TODO Auto-generated constructor stub
-	}
 
 }
